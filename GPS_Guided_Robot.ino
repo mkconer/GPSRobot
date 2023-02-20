b1d239175f322111da4630eeea10602b7e4e5bb1
Mark Conner <mkconner@yahoo.com>
2023-02-19 22:32:13 -0500

    Update GPS_Guided_Robot.ino

--- GPS_Guided_Robot.ino
+++ GPS_Guided_Robot.ino
@@ -6,7 +6,19 @@
 // You can find the Instructable here
 // http://www.instructables.com/id/How-to-Build-a-GPS-Guided-Robot/
 // The Robot requires an Arduino Mega, L293D motor controller, HMC5883L magnetometer, Ublox 6m GPS, and an HC-06 Bluetooth module
-// Remember to run the HMC5883L compass calibration test and enter the results into the code below under void Setup() compass.setOffset(0,0);      
+// Remember to run the HMC5883L compass calibration test and enter the results into the code below under void Setup() compass.setOffset(0,0); 
+
+// If you are having problem with the compass Check the I2C address, mine is 0x1E
+// You may need to connect the compass to the Arduino and run a I2C Scanner Sketch to check the I2C address
+// The HMC5883L chip used in the compass module has a fixed I2C address of 0x1E. 
+// This means that the Jarzebski HMC5883L library assumes that the compass module is connected to the I2C bus with this address, 
+// and the library's code is written accordingly.
+// In the library's source code, you can find the line that sets the I2C address to 0x1E in the HMC5883L.h header file:
+// #define HMC5883L_I2CADDR 0x1E
+// This sets the I2C address to 0x1E, which is the default address of the HMC5883L chip used in the compass module.
+// If your compass module (i.e. qmc5883) has a different I2C address
+// you can modify this value in the header file to match the address of your module. 
+// In that case, you need to upload the modified library to the Arduino board in order to use it.
 
 
 #include <AFMotor.h>                                              // the Adafruit Motor Shield Library ver. 1 https://learn.adafruit.com/adafruit-motor-shield/library-install
